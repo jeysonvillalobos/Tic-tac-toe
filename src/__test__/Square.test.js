1bc1edacb2b56bdd41cd3f110792f43a87f2365f
Jeyson Villalobos <jeyson3213@hotmail.com>
2021-04-21 07:38:34 -0500

    Square test

---
+++ src/__test__/Square.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+
+import Square from '../componets/square';
+
+describe("Test <Square />",()=>{
+
+    const square = shallow(<Square />);
+    test("Render Square",()=>{
+        expect(square.length).toEqual(1);
+        expect(square.find('.Square').exists).toBe(true);
+    });
+
+});
+
+
