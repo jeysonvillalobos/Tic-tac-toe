345ec31b1a872957823477d60baabb365ec44ebd
Jeyson Villalobos <jeyson3213@hotmail.com>
2021-04-21 07:31:38 -0500

    Header test pass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './componets/header';
+
 
 function App(){
 
     return (
         <div className="App">
+            <Header />
             <div className="container">
                 <div className="sub_container">
                     <div className="register"></div>
--- src/componets/header/index.js
+++ src/componets/header/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './style.css';
 
-function Header(){
-    return (
-        <div className="Header"></div>
+function Header({ restart, showPopup }){
+    return(
+        <div className="Header">
+            <header className="header">
+                <div className="div_logo">
+                    <img className="logo" src="/images/tic.png" alt=""/>
+                    <h1>Tic tac toe</h1>
+                </div>
+                <ul className="nav_ul">
+                    <li onClick={()=> restart()}>
+                        <img src="/images/update-arrow.png" alt=""/>
+                        <span>Restart</span>
+                    </li>
+                    <li onClick={()=> showPopup()}>
+                        <img src="/images/info.png" alt=""/>
+                        <span>About</span>
+                    </li>
+                </ul>
+            </header>
+        </div>
     );
-};
-
+}
 export default Header;
