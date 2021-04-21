482e6a90dbefe9a07104e316a324be3cee334065
Jeyson Villalobos <jeyson3213@hotmail.com>
2021-04-21 10:07:01 -0500

    Images

--- src/componets/header/index.js
+++ src/componets/header/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import './style.css';
 import './style.responsive.css';
 
+import logo from '../../assets/tic.png';
+import restartImg from '../../assets/update-arrow.png';
+import about from '../../assets/info.png';
+
 function Header({ restart, showPopup }){
     return(
         <div className="Header">
             <header className="header">
                 <div className="div_logo">
-                    <img className="logo" src="/images/tic.png" alt=""/>
+                    <img className="logo" src={logo} alt=""/>
                     <h1>Tic tac toe</h1>
                 </div>
                 <ul className="nav_ul">
                     <li onClick={()=> restart()}>
-                        <img src="/images/update-arrow.png" alt=""/>
+                        <img src={restartImg} alt=""/>
                         <span className="item">Restart</span>
                     </li>
                     <li onClick={()=> showPopup()}>
-                        <img src="/images/info.png" alt=""/>
+                        <img src={about} alt=""/>
                         <span className="item">About</span>
                     </li>
                 </ul>
