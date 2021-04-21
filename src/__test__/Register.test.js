c25d9e190eec9152830a8f795c6cd0fc4d7c02bb
Jeyson Villalobos <jeyson3213@hotmail.com>
2021-04-21 07:50:53 -0500

    Register test

---
+++ src/__test__/Register.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Register from '../componets/register';
+
+describe('Test <Register />',()=>{
+    const register = shallow(<Register />);
+
+    test('Render Register',()=>{
+        expect(register.length).toEqual(1);
+    });
+
+    test('Render Register UI',()=>{
+        expect(register.find('.register_header').exists()).toBe(true);
+        expect(register.find('.register_data').exists()).toBe(true);
+    })
+
+
+});
